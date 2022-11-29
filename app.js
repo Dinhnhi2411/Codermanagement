07f7adf736c2afcbb2b40b32822ec609773182f1
Đinh Ngọc Liên Nhi <dinhngocliennhi1999@gmail.com>
2022-11-29 14:47:58 +0700

    29/11

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require(
+    "dotenv"
+).config();
+const cors= require("cors");
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
+var indexRouter = require('./src/routes/index');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -13,8 +17,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
+/* DB connection*/
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log(`DB connected ${mongoURI}`))
+  .catch((err) => console.log(err));
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 module.exports = app;
